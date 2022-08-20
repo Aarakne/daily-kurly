37c9b8a9f1be2628c2be07d2f28ea63d90a70b8e
dev-seomoon <seomoon.frontend@gmail.com>
2022-08-20 09:42:25 +0900

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import { Query, QueryClient, QueryClientProvider } from "react-query";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
