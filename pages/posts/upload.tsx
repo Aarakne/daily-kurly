ae0415244268421955b5b76ffe1337288ca8104d
dev-seomoon <seomoon.frontend@gmail.com>
2022-08-22 08:53:23 +0900

    이미지 업로드 폼 디자인

--- pages/posts/upload.tsx
+++ pages/posts/upload.tsx
@@ -3,21 +3,32 @@ import Header from '../../components/public/Header'
 import styled from '@emotion/styled'
 import { FormEventHandler, useState } from 'react'
 import Image from 'next/image'
+import FolderAddIcon from '../../assets/folder-add.svg'
 
-const IMAGE_SIZE = 100
+const IMAGE_SIZE = 80
 
 const Form = styled.form`
   padding: 20px 25px;
   margin-top: 30px;
 `
 
+const FileForm = styled.div`
+  display: flex;
+  overflow: scroll-x;
+`
+
 const FileInput = styled.label`
-  display: block;
   width: ${IMAGE_SIZE}px;
   height: ${IMAGE_SIZE}px;
 
-  background-color: #e7e6e6;
+  display: flex;
+  flex-shrink: 0;
+  align-items: center;
+  justify-content: center;
 
+  background-color: #fff;
+
+  border: 1px solid #e7e6e6;
   border-radius: 5px;
 `
 
@@ -26,10 +37,15 @@ const HiddenInput = styled.input`
 `
 
 const Preview = styled.div`
-  padding-top: 15px;
   display: flex;
+  padding-left: 10px;
 
   gap: 5px;
+`
+
+const ImageBox = styled.div`
+  width: ${IMAGE_SIZE}px;
+  flex-shrink: 0;
 `
 
 const Posts: NextPage = () => {
@@ -55,21 +71,25 @@ const Posts: NextPage = () => {
     <div>
       <Header title="게시글 작성" left="<" right="my" />
       <Form>
-        <FileInput htmlFor="upload-image" onChange={onChangeImages}>
-          <HiddenInput type="file" multiple id="upload-image" />
-        </FileInput>
-        <Preview>
-          {imageUrls.map((imageUrl) => (
-            <Image
-              key={imageUrl}
-              src={imageUrl}
-              alt="preview"
-              width={IMAGE_SIZE}
-              height={IMAGE_SIZE}
-              style={{ borderRadius: 5 }}
-            />
-          ))}
-        </Preview>
+        <FileForm>
+          <FileInput htmlFor="upload-image" onChange={onChangeImages}>
+            <HiddenInput type="file" multiple id="upload-image" />
+            <FolderAddIcon />
+          </FileInput>
+          <Preview>
+            {imageUrls.map((imageUrl) => (
+              <ImageBox key={imageUrl}>
+                <Image
+                  src={imageUrl}
+                  alt="preview"
+                  width={IMAGE_SIZE}
+                  height={IMAGE_SIZE}
+                  style={{ borderRadius: 5 }}
+                />
+              </ImageBox>
+            ))}
+          </Preview>
+        </FileForm>
       </Form>
     </div>
   )
